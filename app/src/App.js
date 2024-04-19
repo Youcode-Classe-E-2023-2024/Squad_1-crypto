c9a752f7ecbfa8d6e3ded8e5eb3a0854515ff644
Mouhcine Essafi <messafi1337@gmail.com>
2024-04-19 11:45:21 +0100

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MarketList from './components/MarketList';
+import AssetList from './components/AssetList';
+import ExchangeList from './components/ExchangeList';
+import MarketDetail from './components/MarketDetail';
+import AssetDetail from './components/AssetDetail';
+import ExchangeDetail from './components/ExchangeDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={MarketList} />
+        <Route exact path="/assets" component={AssetList} />
+        <Route exact path="/exchanges" component={ExchangeList} />
+        <Route exact path="/market/:id" component={MarketDetail} />
+        <Route exact path="/asset/:id" component={AssetDetail} />
+        <Route exact path="/exchange/:id" component={ExchangeDetail} />
+      </Switch>
+    </Router>
   );
 }
 

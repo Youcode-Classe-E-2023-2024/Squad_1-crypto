da455405a000dc8c0d9fa49b5623ae4125bb774c
Elgadisalma <elgadi.salma0@gmail.com>
2024-04-22 11:40:20 +0100

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,36 +1,23 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import { Switch } from 'react-router-dom';
-// import MarketList from './components/MarketList';
-// import AssetList from './components/AssetList';
-// import ExchangeList from './components/ExchangeList';
+import MarketList from './components/MarketList';
+import AssetList from './components/AssetList';
+import ExchangeList from './components/ExchangeList';
 import AssetDetail from './components/AssetDetail';
-// import ExchangeDetail from './components/ExchangeDetail';
-// import { Switch } from 'react-router-dom';
-
+import ExchangeDetail from './components/ExchangeDetail';
 
 function App() {
   return (
-    // <Router>
-    //   {/* navbar */}
-    //   <Routes>
-    //     {/* <Route  path="/" element={<MarketList />} /> */}
-    //     {/* <Route  path="/assets" element={AssetList} /> */}
-    //     {/* <Route  path="/exchanges" element={ExchangeList} /> */}
-    //     {/* <Route  path="/asset/:id" element={AssetDetail} /> */}
-    //     {/* <Route  path="/exchange/:id" element={ExchangeDetail} /> */}
-    //   </Routes>
-    // </Router>
-
-    // <>
-    //   <h1 className="text-3xl font-bold underline bg-red-500 border-2 border-blue-500" >
-    //     hello mohammed
-    //   </h1>
-    // </>
-    <AssetDetail/>
-    
-   );
+    <Router>
+      <Routes>
+        <Route path="/" element={<MarketList />} />
+        <Route path="/assets" element={<AssetList />} />
+        <Route path="/exchanges" element={<ExchangeList />} />
+        <Route path="/asset/:id" element={<AssetDetail />} />
+        <Route path="/exchange/:id" element={<ExchangeDetail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
